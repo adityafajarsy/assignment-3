602353ae3e016a5e25d67a3b0f273cb90b710d9d
adityafajarsy <adityafajar.sy90@gmail.com>
2024-10-11 17:19:00 +0700

    mengubah ex 2

--- nomer2.js
+++ nomer2.js
@@ -1,13 +1,13 @@
 // ADITYA FAJAR SATYA YUDHA
 
 function threeStepsAB(text) {
-    if(text.length < 4) {
+    if(text.length < 3) {
         return false;
     }
     
-    for (let i = 0; i < text.length - 3; i++) {
-        if ((text[i] === 'a' && text[i + 3] === 'b') || 
-            (text[i] === 'b' && text[i + 3] === 'a')) {
+    for (let i = 0; i < text.length - 2; i++) {
+        if ((text[i] === 'a' && text[i + 2] === 'b') || 
+            (text[i] === 'b' && text[i + 2] === 'a')) {
             return true;
         }
     }
